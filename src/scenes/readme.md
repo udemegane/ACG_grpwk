1604f53b375a2e3841e77d2088e42e27b2979fe7
takuto <takutoitoi914@ruri.waseda.jp>
2021-11-30 02:28:14 +0900

    temp: lint all files

--- src/scenes/readme.md
+++ src/scenes/readme.md
@@ -1,4 +1,5 @@
 # Scenes folder
 
 ## Introduction
+
 This folder is generated automatically by the editor. This will containg all the scene's scripts attached to objects.
--- src/scenes/scripts-map.ts
+++ src/scenes/scripts-map.ts
@@ -1,15 +1,15 @@
-import { ScriptMap } from "./tools";
+import { ScriptMap } from './tools';
 
 /**
  * Defines the interface that exposes all exported scripts in this project.
  */
 export interface ISceneScriptMap extends ScriptMap {
-	"src/scenes/scene/camera.ts": any;
+  'src/scenes/scene/camera.ts': any;
 }
 
 /**
  * Defines the map of all available scripts in the project.
  */
 export const scriptsMap: ISceneScriptMap = {
-	"src/scenes/scene/camera.ts": require("./scene/camera"),
-}
+  'src/scenes/scene/camera.ts': require('./scene/camera'),
+};
