3856f7a051d702ceec60fc59b8311b7dfc74a87e
Yu Yanagida <cappuccino1204.ck@gmail.com>
2021-12-11 09:20:16 +0900

    ランタイム用デバッグメニュー追加

--- src/scenes/GameScripts/gameModeBase.ts
+++ src/scenes/GameScripts/gameModeBase.ts
@@ -1,6 +1,6 @@
 import { Scene } from '@babylonjs/core';
+import { Pane } from 'tweakpane';
 import { visibleInInspector } from '../decorators';
-
 /**
  * This represents a script that is attached to a node in the editor.
  * Available nodes are:
@@ -22,6 +22,11 @@ import { visibleInInspector } from '../decorators';
 export default class SceneScriptBase extends Scene {
   @visibleInInspector('string', 'In gameModeBase', 'Hello World!')
   private _testString: string;
+
+  @visibleInInspector('boolean', 'Show Debug menu', true)
+  private _isVisibleDebugMenu: boolean;
+
+  private pane: Pane;
   /**
    * Override constructor.
    * @warn do not fill.
@@ -33,7 +38,24 @@ export default class SceneScriptBase extends Scene {
    * Called on the node is being initialized.
    * This function is called immediatly after the constructor has been called.
    */
+  private setDebugMenu(): void {
+    this.pane = new Pane();
+    const PARAMS = {
+      factor: 123,
+      title: 'hello',
+      color: '#0f0',
+    };
+
+    this.pane.addInput(PARAMS, 'factor');
+    this.pane.addInput(PARAMS, 'title');
+    this.pane.addInput(PARAMS, 'color');
+  }
+
   public onInitialize(): void {
+    if (this._isVisibleDebugMenu) {
+      this.setDebugMenu();
+    }
+
     // ...
   }
 
